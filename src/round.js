324ce0d3edab98a3765ac915b17274382c6506e6
Jesse R Mykolyn <jesse.mykolyn@groupbyinc.com>
2019-01-19 01:19:56 -0500

    Setup Round#play()

--- src/round.js
+++ src/round.js
@@ -1,8 +1,40 @@
+const Player = require('./player');
+
 class Round {
   constructor(player1, player2) {
     this.isComplete = false;
     this.isTie = false;
     this.players = player1 && player2 ? [player1, player2] : [];
+  }
+
+  play() {
+    if (
+      !this.players.length
+      || !this.players.every((player) => player instanceof Player)
+    ) {
+      return;
+    }
+
+    const results = this.players.map((player) => player.play());
+    const opts = ['rock', 'paper', 'scissors'];
+
+    this.isComplete = true;
+
+    if (results[0] === results[1]) {
+      this.isTie = true;
+      return;
+    }
+
+    if (results[0] === opts[0] && results[1] === opts[opts.length - 1]) {
+      this.winner = this.players[0];
+      this.loser = this.players[1];
+    } else if (opts.indexOf(results[0]) > opts.indexOf(results[1])) {
+      this.winner = this.players[0];
+      this.loser = this.players[1];
+    } else {
+      this.winner = this.players[1];
+      this.loser = this.players[0];
+    }
   }
 }
 
