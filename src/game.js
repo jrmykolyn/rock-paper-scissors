ce82e105e9fb25b1b67b778e50b752ff7355bbe2
Jesse R Mykolyn <jesse.mykolyn@groupbyinc.com>
2019-01-20 22:18:47 -0500

    Refactor Game#play()

--- src/game.js
+++ src/game.js
@@ -17,12 +17,22 @@ class Game {
 
       // Check for wins.
       if (this.rounds.length >= 3) {
-        if (this.rounds.filter(({ winner }) => winner === this.players[0]).length === 3) {
+        const { player1: player1Wins, player2: player2Wins } = this.rounds.reduce((acc, { winner }) => {
+          if (winner === this.players[0]) {
+            acc.player1 = (acc.player1 + 1 || 1);
+          } else {
+            acc.player2 = (acc.player2 + 1 || 1);
+          }
+
+          return acc;
+        }, {});
+
+        if (player1Wins === 3) {
           this.winner = this.players[0];
           this.loser = this.players[1];
           this.isComplete = true;
           break;
-        } else if (this.rounds.filter(({ winner }) => winner === this.players[1]).length === 3) {
+        } else if (player2Wins === 3) {
           this.winner = this.players[1];
           this.loser = this.players[0];
           this.isComplete = true;
